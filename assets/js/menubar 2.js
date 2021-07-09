6f993b071af7e2e42e594ff486a9c4b67f97e584
omotomasaya <omotomasayaarai@gmail.com>
2021-07-09 16:25:27 +0900

    Revert "first commit"
    This reverts commit 141d1ee0d0fd1c9fe854128bf84751f27b4ed4f8.

--- assets/js/menubar 2.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-        $(document).ready(function(){
-  $('#contextmenu > div').next().hide();
-  $('#contextmenu > div').click(function(){
-    $(this).next().slideToggle('slow');
-  });
-});
-    });
--- core/database/connection.php
+++ core/database/connection.php
@@ -1,18 +1,16 @@
 <?php
-function dbConnect(){
-  $dsn = 'mysql:host=us-cdbr-east-04.cleardb.com; dbname=heroku_dc045a7b9996d70';
-  $user = 'be55cc2f486f93';
-  $pass = 'd253e777';
+  $dsn = 'mysql:host=localhost; dbname=twitterclone';
+  $user = 'root';
+  $pass = 'root';
 
   try {
     $pdo = new PDO($dsn, $user, $pass, [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
-    
   } catch(PDOException $e){
     echo '接続失敗です'. $e->getMessage();
     exit();
   }
-}
+
 ?>
