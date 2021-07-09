b7647d0f7100ee19bbaeb44b982193704da36097
omotomasaya <omotomasayaarai@gmail.com>
2021-07-09 18:52:14 +0900

    修正

--- core/database/connection.php
+++ core/database/connection.php
@@ -1,11 +1,9 @@
 <?php
   require_once('env.php');
 
-  $db = parse_url($_SERVER['mysql://be55cc2f486f93:d253e777@us-cdbr-east-04.cleardb.com/heroku_dc045a7b9996d70?reconnect=true']);
-  $db['heroku_dc045a7b9996d70'] = ltrim($db['path'], '/');
-  $dsn = "mysql:host={$db['us-cdbr-east-04.cleardb.com']};dbname={$db['heroku_dc045a7b9996d70']};charset=utf8";
+  $dsn = "mysql:host={$db['us-cdbr-east-04.cleardb.com']}; dbname={$db['heroku_dc045a7b9996d70']}";
   $user = $db['be55cc2f486f93'];
-  $password = $db['d253e777'];
+  $pass = $db['d253e777'];
 
   try {
     $pdo = new PDO($dsn, $user, $pass, [
